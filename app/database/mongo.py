9e4db2281d6d999d40b0b25585e76bfb6eeed247
Harsh Singh <harshsingh25665@gmail.com>
2026-07-09 10:49:12 +0530

    Initialize MongoDB client and collections

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -1,1 +1,14 @@
+import os
 
+from pymongo import MongoClient
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+db = client["atlas"]
+
+positions_collection = db["positions"]
+trades_collection = db["trades"]
+run_lock_collection = db["run_lock"]
+settings_collection = db["settings"]
